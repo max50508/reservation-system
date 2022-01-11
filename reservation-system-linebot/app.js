a7d58391d91d79de0cfdc7732ab4ead01c9d140c
Quan-Zhe <a0906397408@gmail,com>
2022-01-12 02:45:53 +0800

    update

--- reservation-system-linebot/app.js
+++ reservation-system-linebot/app.js
@@ -1,25 +1,40 @@
-// 引用linebot SDK
-var linebot = require('linebot');
-
-// 用於辨識Line Channel的資訊
-var bot = linebot({
-  CHANNEL_ID: '1655943690',
-  CHANNEL_SECRET: '0e3f4af08645b75b0e10e0d46c1c4a4e',
-  CHANNEL_ACCESS_TOKEN: '6XrwJ8BH9qnAsW+kxngzD8nTwcd/+CsbyIq67r1t59zOdron0DhljJfvwyASEJf1AdniGGjmHXODVlUTTVDE6MP1OllAoeSZjPvzqlnIVKEMgIcfAu7sJHtbmvgoAx8gZ5ltVLJuftuLdxyg9hUBGgdB04t89/1O/w1cDnyilFU='
+// index.js
+const line = require('@line/bot-sdk');
+var express = require('express');
+const config = {
+  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
+  channelSecret: 'YOUR_CHANNEL_SECRET'
+};
+// create LINE SDK client
+const client = new line.Client(config);
+// create Express app
+// about Express itself: <https://expressjs.com/>
+const app = express();
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+	console.log(req, res)
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
-
-// 當有人傳送訊息給Bot時
-bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-  event.reply(event.message.text).then(function (data) {
-    // 當訊息成功回傳後的處理
-  }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
-  });
-});
-
-// Bot所監聽的webhook路徑與port
-bot.listen('/linewebhook', 3000, function () {
-    console.log('[BOT已準備就緒!!], heroku部屬成功!');
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
